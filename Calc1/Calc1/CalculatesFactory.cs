8dfb3fa731634c2e339cf03cfc54d925edcec3f0
TitusPullo <ruslanholovms@gmail.com>
2017-07-04 16:17:50 +0600

    Factory

---
+++ Calc1/Calc1/CalculatesFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace Calc1
+{
+    public static class CalculatesFactory
+    {
+        static ICalculate createCalculator(string calcName)
+        {
+            ICalculate calculate;
+            switch (calcName)
+            {
+                case "button1":
+                    calculate = new Addition();
+                    break;
+                case "button2":
+                    calculate = new Substraction();
+                    break;
+                case "button3":
+                    calculate = new Multiplication();
+                    break;
+                case "button4":
+                    calculate = new Division();
+                    break;
+                default:
+                    calculate = null;
+                    break;
+            }
+            return calculate;
+        }
+    }
+}
